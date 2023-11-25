96afb2d3f8351c084fa3c2bdeb060770fb6c52e9
BeautyAndHomeLover <151921481+BeautyAndHomeLover@users.noreply.github.com>
2023-11-25 18:19:54 +0100

    Create processa_form.php

---
+++ processa_form.php
@@ -0,0 +1,29 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $tel = $_POST["tel"];
+    $message = $_POST["message"];
+    $contactPreference = $_POST["contact-preference"];
+
+    // Costruisci il corpo del messaggio
+    $mailBody = "Nome: $name\n";
+    $mailBody .= "Email: $email\n";
+    $mailBody .= "Telefono: $tel\n";
+    $mailBody .= "Messaggio: $message\n";
+    $mailBody .= "Preferenza di contatto: $contactPreference\n";
+
+    // Indirizzo email a cui inviare i dati del modulo
+    $to = "tuo-indirizzo-email@tuo-dominio.com";
+    $subject = "Nuovo modulo di contatto da Beauty & Home";
+
+    // Invia l'email
+    mail($to, $subject, $mailBody);
+
+    // Puoi anche reindirizzare l'utente a una pagina di conferma
+    header("Location: conferma_invio.html");
+} else {
+    // Se la richiesta non è POST, reindirizza a una pagina di errore
+    header("Location: errore.html");
+}
+?>
